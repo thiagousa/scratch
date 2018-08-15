a0da11b8e8949c44ded6cb4ae58874b5f22c2d2e
thiagousa <thiago@thiagodsantos.com>
2018-08-15 15:40:12 -0400

    Ajuste do layout com o bootstrap

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,14 +2,26 @@
 
 @section('title', 'Page Title')
 
-@section('navbar')
-
-@endsection
 
 @section('content')
+
+ 
+@foreach ($posts as $post)  
+<div class="blog-post">
+<h2 class="blog-post-title">{{$post->title}}</h2>
+        <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+
+        <p>{{$post->body}}</p>
+        </div><!-- /.blog-post -->
+
+@endforeach 
+      <nav>
+        <ul class="pager">
+          <li><a href="#">Previous</a></li>
+          <li><a href="#">Next</a></li>
+        </ul>
+      </nav>
   
-@endsection
-
-@section('footer')
 
 @endsection
+
